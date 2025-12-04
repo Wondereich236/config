861c9d7cde7e742e835615ee8e41af5801c7f1fd
satella <kiyotakaa591@gmail.com>
2025-12-04 15:46:09 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,10 +3,6 @@
 # Hata olursa scripti durdur
 set -e
 
-echo "Profil dosyası kopyalanıyor..."
 sudo cp ~/git-script/config/profile /etc/profile
 
-echo "bash.bashrc dosyası kopyalanıyor..."
 sudo cp ~/git-script/config/bash.bashrc /etc/bash.bashrc
-
-echo "İşlem tamamlandı."
